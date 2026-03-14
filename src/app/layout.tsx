efcf300c5c468a8a0988b008d400926644976168
Kumaresan <ux.kumaresan@gmail.com>
2026-03-14 17:34:53 +0530

    fix: move font variables to html element so font-sans resolves correctly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={`dark ${geistSans.variable} ${geistMono.variable}`}>
+      <body className="antialiased">
         {children}
       </body>
     </html>
